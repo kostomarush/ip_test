99a67e8150476a3e72b391612bb00d33652099b6
kostomarush <vlad.kostomarov2013@gmail.com>
2023-12-18 19:26:19 +0300

    qweq

--- ip_add.py
+++ ip_add.py
@@ -3,16 +3,31 @@ import json
 from random import shuffle, choice
 import json
 import sys
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
 from graph import Ui_MainWindow
+from info import Ui_Form
 
-class mywindow(QtWidgets.QMainWindow):
+
+app1 = QtWidgets.QApplication(sys.argv)
+app1.setWindowIcon(QtGui.QIcon('icon.ico'))
+
+class Information(QtWidgets.QMainWindow):
     def __init__(self):
-        super(mywindow, self).__init__()
+        super(Information, self).__init__()
+        self.ui2 = Ui_Form()
+        self.ui2.setupUi(self)
+        
+
+class Mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Mywindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.add_checked_checkboxes_to_textedit)
         self.ui.pushButton_2.clicked.connect(self.result)
+        self.ui.pushButton_3.clicked.connect(self.clear)
+        self.ui.action.triggered.connect(self.read_and_shuffle)
+        self.ui.action_2.triggered.connect(self.open_info)
         self.count = 0
         
     def read_and_shuffle(self):
@@ -24,15 +39,13 @@ class mywindow(QtWidgets.QMainWindow):
 
         ip_pairs_list = choice(data["ip_pairs"])
         shuffle(ip_pairs_list)
-        print(json.dumps(ip_pairs_list, indent=2))
         for item in ip_pairs_list:
             enter_add.append(item["ip1"])
             enter_add.append(item["ip2"])
         shuffle(enter_add)
-        print(enter_add)
         self.items = []
-        # Перемешиваем все IP-адреса
-        #Вывод всех IP-адресов в случайном порядке
+
+
         self.ui.checkBox_3.setText(f'{enter_add[0]}')
         self.ui.checkBox_4.setText(f'{enter_add[1]}')
         self.ui.checkBox_7.setText(f'{enter_add[2]}')
@@ -52,27 +65,48 @@ class mywindow(QtWidgets.QMainWindow):
 
     def add_checked_checkboxes_to_textedit(self):
         
+        checked_items = [checkbox.text() for checkbox in self.findChildren(QtWidgets.QCheckBox) if checkbox.isEnabled() and checkbox.isChecked()]
+        
+        if len(checked_items) > 2 or len(checked_items) < 2:
+            QtWidgets.QMessageBox.critical(self.ui.centralwidget, "Ошибка", "Выберите два чекбокса.")
+            return
+        
         [checkbox.setDisabled(True) for checkbox in self.findChildren(QtWidgets.QCheckBox) if checkbox.isChecked()]
         
-        checked_items = [checkbox.text() for checkbox in self.findChildren(QtWidgets.QCheckBox) if checkbox.isChecked()]
-        
-
-        # Получаем текст из отмеченных чекбоксов и добавляем в QTextEdit
         if checked_items:
             if self.are_networks_in_same_subnet(checked_items[0], checked_items[1]):
                 self.count += 1
-            else:
-                pass
+        
+        all_checkboxes_disabled = all(not checkbox.isEnabled() for checkbox in self.findChildren(QtWidgets.QCheckBox))
+        
+        if all_checkboxes_disabled:
 
+
+            self.ui.pushButton_2.setEnabled(True)
+
+        self.ui.statusbar.showMessage(f'Значение добавлено')
+        
     
     def result(self):
         QtWidgets.QMessageBox.information(self.ui.centralwidget, "Результат", f"Ваша оценка {self.count}")
-        return
-            
+        QtCore.QCoreApplication.quit()
+    
+    def clear(self):
+        self.count = 0
+        for checkbox in self.findChildren(QtWidgets.QCheckBox):
+            checkbox.setEnabled(True)
+            checkbox.setChecked(False)
+
+        self.ui.statusbar.showMessage(f'Все значения удалены')
+        
+    def open_info(self):
+        self.second_window = Information()
+        self.second_window.show()
+        
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    w = mywindow()
+    w = Mywindow()
     w.read_and_shuffle()
     w.show()
     sys.exit(app.exec_())
